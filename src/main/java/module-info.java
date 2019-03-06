aa9696597c5dcc397159f09aefb4b064f93f5777
Brian Schlining <bschlining@gmail.com>
2019-03-05 20:49:20 -0800

    Running tests with jlink. Issue #1

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module jsharktopoda {
+  requires com.jfoenix;
+  requires de.jensd.fx.fontawesomefx.commons;
+  requires de.jensd.fx.fontawesomefx.materialicons;
+  requires java.prefs;
+  requires javafx.controls;
+  requires javafx.fxml;
+  requires javafx.media;
+  requires javafx.swing;
+  requires vlcj;
+
+  exports org.mbari.m3.jsharktopoda.javafx;
+}
