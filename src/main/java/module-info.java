d2110d544b9e6d06fe874b8a7ca49a77013451b3
Brian Schlining <bschlining@gmail.com>
2019-03-06 16:34:27 -0800

    Removed fontawesomefx

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,17 @@
 module jsharktopoda {
   requires com.jfoenix;
-  requires de.jensd.fx.fontawesomefx.commons;
-  requires de.jensd.fx.fontawesomefx.materialicons;
+  // requires de.jensd.fx.fontawesomefx.commons;
+  // requires de.jensd.fx.fontawesomefx.materialicons;
   requires java.prefs;
   requires java.sql;
   requires javafx.controls;
   requires javafx.fxml;
   requires javafx.media;
   requires javafx.swing;
+  requires org.kordamp.iconli.core;
+  requires org.kordamp.ikonli.javafx;
+  // add icon pack modules
+  requires org.kordamp.ikonli.material;
   requires vlcj;
 
   exports org.mbari.m3.jsharktopoda.javafx;
---
+++ src/main/java/org/mbari/m3/jsharktopoda/javafx/MaterialIcons.java
@@ -0,0 +1,31 @@
+package org.mbari.m3.jsharktopoda.javafx;
+
+import org.kordamp.ikonli.javafx.FontIcon;
+import org.kordamp.ikonli.material.Material;
+
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
+/**
+ * MaterialIcons
+ */
+public class MaterialIcons {
+
+  private MaterialIcons() {
+  }
+
+  private static final Color color = Color.BLACK;
+
+
+  public static final Text POWER_SETTINGS_NEW = FontIcon.of(Material.POWER_SETTINGS_NEW, 30, color);
+  public static final Text SETTINGS = FontIcon.of(Material.SETTINGS, 30, color);
+  public static final Text COMPUTER = FontIcon.of(Material.COMPUTER, 30, color);
+  public static final Text CLOUD = FontIcon.of(Material.CLOUD, 30, color);
+  public static final Text VOLUME_UP = FontIcon.of(Material.VOLUME_UP, 20, color);
+  public static final Text VOLUME_DOWN= FontIcon.of(Material.VOLUME_DOWN, 20, color);
+  public static final Text FAST_FORWARD = FontIcon.of(Material.FAST_FORWARD, 30, color);
+  public static final Text FAST_REWIND = FontIcon.of(Material.FAST_REWIND, 30, color);
+  public static final Text PLAY_ARROW_50PX = FontIcon.of(Material.PLAY_ARROW, 50, color);
+  public static final Text PLAY_ARROW = FontIcon.of(Material.PLAY_ARROW, 30, color);
+  public static final Text PAUSE = FontIcon.of(Material.PAUSE, 30, color);
+}
