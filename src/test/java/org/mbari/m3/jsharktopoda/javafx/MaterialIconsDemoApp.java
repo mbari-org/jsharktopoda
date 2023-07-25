33ccca1be8ff0c961bd37ce7ff844e7aa92e51d7
Brian Schlining <bschlining@gmail.com>
2023-07-25 14:53:45 -0700

    upgrade to gradle 8.2.1. Removed obsolete tests

--- src/test/java/org/mbari/m3/jsharktopoda/javafx/MaterialIconsDemoApp.java
+++ src/test/java/org/mbari/m3/jsharktopoda/javafx/MaterialIconsDemoApp.java
@@ -1,7 +1,6 @@
 package org.mbari.m3.jsharktopoda.javafx;
 
-import de.jensd.fx.glyphs.materialicons.MaterialIcon;
-import de.jensd.fx.glyphs.materialicons.utils.MaterialIconFactory;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
@@ -16,14 +15,14 @@ public class MaterialIconsDemoApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FlowPane iconsPane = new FlowPane(3,3);
-        for (MaterialIcon icon : MaterialIcon.values()) {
-            iconsPane.getChildren().add(MaterialIconFactory.get().createIcon(icon, "3em"));
-        }
-        Scene scene = new Scene(new ScrollPane(iconsPane), 500, 500);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("FontAwesomeFX: MaterialIcons Demo: " + MaterialIcon.values().length + " Icons");
-        primaryStage.show();
+//        FlowPane iconsPane = new FlowPane(3,3);
+//        for (MaterialIcons icon : MaterialIcon.values()) {
+//            iconsPane.getChildren().add(MaterialIconFactory.get().createIcon(icon, "3em"));
+//        }
+//        Scene scene = new Scene(new ScrollPane(iconsPane), 500, 500);
+//        primaryStage.setScene(scene);
+//        primaryStage.setTitle("FontAwesomeFX: MaterialIcons Demo: " + MaterialIcon.values().length + " Icons");
+//        primaryStage.show();
 
     }
 
